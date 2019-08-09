1422ad83006c05f138ad758bc139f0f29ae8c3fc
Jules White <jules.white@gmail.com>
2019-08-09 14:46:43 -0500

    Updated tests to match new id type

--- src/test/java/edu/vanderbilt/wall/wall/WallApplicationTests.java
+++ src/test/java/edu/vanderbilt/wall/wall/WallApplicationTests.java
@@ -15,7 +15,8 @@ import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -24,7 +25,8 @@ import org.springframework.test.web.servlet.ResultActions;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(WallController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class WallApplicationTests {
 
 	@Autowired
